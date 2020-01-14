ad8c8ebbda5dff6c6dc8eda278a154ef12bc5943
Joseph Dyer <joedyer1989@gmail.com>
2020-01-14 07:43:46 +0000

    updated 'npm run' commands

--- resources/js/modules/lazyload/index.js
+++ resources/js/modules/lazyload/index.js
@@ -12,8 +12,7 @@ const Lazyload = function Lazyload()
 
         // options
         const options = {
-            threshold: 0,
-            rootMargin: '0px 0px 100px 0px'
+            threshold: 0.5
         }
 
         const preloadImage = function preloadImage(img) {
